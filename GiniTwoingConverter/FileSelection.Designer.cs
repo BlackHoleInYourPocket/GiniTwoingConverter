054a92720466235efaf943bfe263481cfedb7760
Seyfi Can Zeyrek <seyficanzeyrek@gmail.com>
2020-06-09 22:21:47 +0300

    First Commits

---
+++ GiniTwoingConverter/FileSelection.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace GiniTwoingConverter
+{
+	partial class FileSelection
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtPath = new System.Windows.Forms.TextBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.txtInfo = new System.Windows.Forms.TextBox();
+			this.btnTwoing = new System.Windows.Forms.Button();
+			this.btnGini = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtPath
+			// 
+			this.txtPath.AcceptsReturn = true;
+			this.txtPath.AllowDrop = true;
+			this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtPath.Location = new System.Drawing.Point(18, 50);
+			this.txtPath.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtPath.Multiline = true;
+			this.txtPath.Name = "txtPath";
+			this.txtPath.ReadOnly = true;
+			this.txtPath.Size = new System.Drawing.Size(494, 90);
+			this.txtPath.TabIndex = 0;
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnBrowse.Location = new System.Drawing.Point(524, 50);
+			this.btnBrowse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(154, 92);
+			this.btnBrowse.TabIndex = 1;
+			this.btnBrowse.Text = "BROWSE";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+			// 
+			// openFileDialog
+			// 
+			this.openFileDialog.FileName = "openFileDialog1";
+			// 
+			// txtInfo
+			// 
+			this.txtInfo.AcceptsReturn = true;
+			this.txtInfo.AcceptsTab = true;
+			this.txtInfo.AllowDrop = true;
+			this.txtInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+			| System.Windows.Forms.AnchorStyles.Left)
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtInfo.Location = new System.Drawing.Point(18, 167);
+			this.txtInfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtInfo.Multiline = true;
+			this.txtInfo.Name = "txtInfo";
+			this.txtInfo.ReadOnly = true;
+			this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtInfo.Size = new System.Drawing.Size(494, 200);
+			this.txtInfo.TabIndex = 2;
+			// 
+			// btnTwoing
+			// 
+			this.btnTwoing.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnTwoing.Enabled = false;
+			this.btnTwoing.Location = new System.Drawing.Point(524, 167);
+			this.btnTwoing.Name = "btnTwoing";
+			this.btnTwoing.Size = new System.Drawing.Size(154, 80);
+			this.btnTwoing.TabIndex = 3;
+			this.btnTwoing.Text = "Open Twoing File";
+			this.btnTwoing.UseVisualStyleBackColor = true;
+			this.btnTwoing.Click += new System.EventHandler(this.BtnTwoing_Click);
+			// 
+			// btnGini
+			// 
+			this.btnGini.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnGini.Enabled = false;
+			this.btnGini.Location = new System.Drawing.Point(524, 287);
+			this.btnGini.Name = "btnGini";
+			this.btnGini.Size = new System.Drawing.Size(154, 80);
+			this.btnGini.TabIndex = 4;
+			this.btnGini.Text = "Open Gini File";
+			this.btnGini.UseVisualStyleBackColor = true;
+			this.btnGini.Click += new System.EventHandler(this.BtnGini_Click);
+			// 
+			// FileSelection
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(705, 391);
+			this.Controls.Add(this.btnGini);
+			this.Controls.Add(this.btnTwoing);
+			this.Controls.Add(this.txtInfo);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.txtPath);
+			this.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "FileSelection";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "DEUCENG - ML Classification Tool";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtPath;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.OpenFileDialog openFileDialog;
+		private System.Windows.Forms.TextBox txtInfo;
+		private System.Windows.Forms.Button btnTwoing;
+		private System.Windows.Forms.Button btnGini;
+	}
+}
+
---
+++ GiniTwoingConverter/FileSelection.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using weka.core;
+using weka.core.converters;
+
+namespace GiniTwoingConverter
+{
+    public partial class FileSelection : Form
+    {
+        #region [VARIABLES]
+        private List<string[]> categories;
+        private string twoingPath = Directory.GetCurrentDirectory() + @"/Twoing.arff";
+        private string giniPath = Directory.GetCurrentDirectory() + @"/Gini.arff";
+        #endregion
+
+        public FileSelection()
+        {
+            InitializeComponent();
+        }
+
+
+        #region [PRIVATE EVENTS]
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            categories = new List<string[]>();
+            txtInfo.Text = "";
+            btnTwoing.Enabled = false;
+            btnGini.Enabled = false;
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtPath.Text = openFileDialog.FileName;
+                btnTwoing.Enabled = true;
+                btnGini.Enabled = true;
+            }
+            weka.core.Instances insts = new weka.core.Instances(new java.io.FileReader(txtPath.Text));
+            Discreatization(insts);
+        }
+
+        private void BtnTwoing_Click(object sender, EventArgs e)
+        {
+            Process.Start(twoingPath);
+        }
+
+        private void BtnGini_Click(object sender, EventArgs e)
+        {
+            Process.Start(giniPath);
+        }
+        #endregion
+
+
+        #region [PRIVATE METHODS]
+
+        /// <summary>
+        ///  Discreatize instance if the instance is numeric
+        /// </summary>
+        /// <param name="insts"></param>
+        private void Discreatization(weka.core.Instances insts)
+        {
+            weka.filters.unsupervised.attribute.Discretize myDiscretize = new
+                weka.filters.unsupervised.attribute.Discretize();
+            myDiscretize.setInputFormat(insts);
+            myDiscretize.setFindNumBins(true);
+            insts = weka.filters.Filter.useFilter(insts, myDiscretize);
+            DataPreparation(insts);
+        }
+
+        /// <summary>
+        /// Determines all categories --> categories
+        /// Determines category numbers of each attributes -->> categoryTypeNumber
+        /// Determines target numbers and amounts of each categories of each attributes  -->> categoryTypeTargetNumber
+        /// [i][j][k] i means attributes, j means  categories, k means targets
+        /// </summary>
+        /// <param name="insts"></param>
+        private void DataPreparation(weka.core.Instances insts)
+        {
+            for (int i = 0; i < insts.numAttributes(); i++)
+            {
+                string[] categoryType = new string[insts.attribute(i).numValues()];
+                for (int j = 0; j < insts.attribute(i).numValues(); j++)
+                {
+                    categoryType[j] = insts.attribute(i).value(j).ToString();
+                }
+                categories.Add(categoryType);
+            }
+
+            List<List<string>> lst = new List<List<string>>();
+
+            for (int i = 0; i < insts.numInstances(); i++)
+            {
+                lst.Add(new List<string>());
+
+                for (int j = 0; j < insts.instance(i).numValues(); j++)
+                {
+                    lst[lst.Count - 1].Add(insts.instance(i).toString(j));
+                }
+            }
+
+            List<int[]> categoryTypeNumber = new List<int[]>();
+            List<int[,]> categoryTypeTargetNumber = new List<int[,]>();
+            for (int i = 0; i < categories.Count; i++)
+            {
+                categoryTypeNumber.Add(new int[categories[i].Length]);
+                categoryTypeTargetNumber.Add(new int[categories[i].Length, categories[categories.Count - 1].Length]);
+            }
+
+            for (int i = 0; i < lst.Count; i++) //Satır
+            {
+                for (int j = 0; j < lst[i].Count; j++)//Sütün
+                {
+                    for (int k = 0; k < categories[j].Length; k++) //Kategori Sayısı
+                    {
+                        string targetValue = lst[i][lst[i].Count - 1];
+                        if (lst[i][j].Contains(categories[j][k]))
+                        {
+                            categoryTypeNumber[j][k] += 1;
+                            for (int trgt = 0; trgt < categories[categories.Count - 1].Length; trgt++)
+                            {
+                                if (targetValue == categories[categories.Count - 1][trgt])
+                                {
+                                    categoryTypeTargetNumber[j][k, trgt] += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Twoing(insts, categoryTypeNumber, categoryTypeTargetNumber);
+            Gini(insts, categoryTypeNumber, categoryTypeTargetNumber);
+            LogInfo("Dataset is saved.\r\n\r\n");
+            LogInfo("TWOING : " + twoingPath + "\r\n\r\n");
+            LogInfo("GINI : " + giniPath + "\r\n");
+        }
+
+
+
+        /// <summary>
+        /// Calculates Pleft, Prigth, rigthTargets , PclassDivideTleft, PclassDivideTleft and teta numbers for each inctance
+        /// Then calling the creating dataset function
+        /// </summary>
+        /// <param name="insts"></param>
+        /// <param name="categoryTypeNumber"></param>
+        /// <param name="categoryTypeTargetNumber"></param>
+        private void Twoing(weka.core.Instances insts, List<int[]> categoryTypeNumber, List<int[,]> categoryTypeTargetNumber)
+        {
+            List<double[]> categoryTetaNumber = new List<double[]>();
+            for (int i = 0; i < categoryTypeNumber.Count; i++)
+            {
+                categoryTetaNumber.Add(new double[categoryTypeNumber[i].Length]);
+            }
+
+            for (int i = 0; i < categoryTypeNumber.Count - 1; i++)
+            {
+                for (int j = 0; j < categoryTypeNumber[i].Length; j++)
+                {
+                    Double pLeft = Convert.ToDouble(categoryTypeNumber[i][j]) / Convert.ToDouble(insts.numInstances());
+                    Double pRight = 1 - pLeft;
+                    Double sumFunction = 0;
+                    for (int k = 0; k < categoryTypeNumber[categoryTypeNumber.Count - 1].Length; k++)
+                    {
+                        Double PclassDivideTleft = Convert.ToDouble(categoryTypeTargetNumber[i][j, k]) / Convert.ToDouble(categoryTypeNumber[i][j]);
+                        int sagtarafıntargetları = 0;
+                        for (int h = 0; h < categoryTypeNumber[i].Length; h++)
+                        {
+                            if (h != j)
+                            {
+                                sagtarafıntargetları += categoryTypeTargetNumber[i][h, k];
+                            }
+                        }
+                        Double PclassDivideTRigt = Convert.ToDouble(sagtarafıntargetları) / Convert.ToDouble((insts.numInstances() - categoryTypeNumber[i][j]));
+                        sumFunction += Math.Abs(PclassDivideTleft - PclassDivideTRigt);
+                    }
+
+                    categoryTetaNumber[i][j] = 2 * pLeft * pRight * sumFunction;
+                }
+            }
+            Instances fileInst = new Instances(insts);
+            CreateNewDataset(fileInst, categoryTetaNumber, twoingPath);
+        }
+
+        /// <summary>
+        /// Calculates giniLeft, giniRigth(these are fraction operations, so also calculates denominator and numerator of fractions)
+        /// then calvulates gini result for each inctance
+        /// Then calling the creating dataset function
+        /// </summary>
+        /// <param name="insts"></param>
+        /// <param name="categoryTypeNumber"></param>
+        /// <param name="categoryTypeTargetNumber"></param>
+        private void Gini(weka.core.Instances insts, List<int[]> categoryTypeNumber, List<int[,]> categoryTypeTargetNumber)
+        {
+            List<double[]> giniResult = new List<double[]>();
+            for (int i = 0; i < categoryTypeNumber.Count; i++)
+            {
+                giniResult.Add(new double[categoryTypeNumber[i].Length]);
+            }
+
+            for (int i = 0; i < categoryTypeNumber.Count - 1; i++)
+            {
+                int changeAttribute = 0;
+
+                for (changeAttribute = 0; changeAttribute < categoryTypeNumber[i].Length; changeAttribute++)
+                {
+                    List<Double> giniLeftNumerator = new List<Double>();
+                    List<Double> giniRigthNumerator = new List<Double>(); //numerator -> pay
+                    Double giniRigthDenominator = 0;  //denominator -> payda
+                    Double giniLeftDenominator = 0;
+                    Double sumFunctionRight = 0;
+                    Double giniRight = 0;
+                    Double giniLeft = 0;
+                    int categoryChange = 0;
+                    Double sumFuntionLeft = 0;
+                    for (categoryChange = 0; categoryChange < categoryTypeNumber[categoryTypeNumber.Count - 1].Length; categoryChange++)
+                    {
+                        giniLeftNumerator.Add(Convert.ToDouble(categoryTypeTargetNumber[i][changeAttribute, categoryChange]));
+                    }
+
+                    for (int index = 0; index < giniLeftNumerator.Count; index++)
+                    {
+                        giniLeftDenominator += giniLeftNumerator[index];
+                    }
+
+                    for (int z = giniLeftNumerator.Count; z > 0; z--)
+                    {
+                        sumFuntionLeft += Convert.ToDouble(Math.Pow(Math.Abs(giniLeftNumerator[z - 1] / giniLeftDenominator), 2));
+                    }
+                    giniLeft = 1 - sumFuntionLeft;
+
+                    int[] targets = new int[giniLeftNumerator.Count];
+                    for (int h = 0; h < categoryTypeNumber[i].Length; h++)
+                    {
+                        if (h != changeAttribute)
+                        {
+                            for (int z = 0; z < giniLeftNumerator.Count; z++)
+                            {
+                                giniRigthNumerator.Add(Convert.ToDouble(categoryTypeTargetNumber[i][h, z]));
+                                targets[z] += categoryTypeTargetNumber[i][h, z];
+                            }
+                        }
+
+                    }
+                    for (int index = 0; index < giniRigthNumerator.Count; index++)
+                    {
+                        giniRigthDenominator += giniRigthNumerator[index];
+                    }
+
+                    for (int z = targets.Length; z > 0; z--)
+                    {
+                        sumFunctionRight += Convert.ToDouble(Math.Pow(Math.Abs(targets[z - 1] / giniRigthDenominator), 2));
+                    }
+                    giniRight = 1 - sumFunctionRight;
+
+                    giniResult[i][changeAttribute] = Convert.ToDouble(1 / (giniLeftDenominator + giniRigthDenominator)) * Convert.ToDouble((giniLeftDenominator * giniLeft) + (giniRigthDenominator * giniRight));
+
+                }
+                Instances fileInst = new Instances(insts);
+                CreateNewDataset(fileInst, giniResult, giniPath);
+            }
+        }
+
+
+        /// <summary>
+        /// Adds teta results of gini results to the list
+        /// Change the attributes of the arff file
+        /// Adds the attributes to arff file
+        /// </summary>
+        /// <param name="insts"></param>
+        /// <param name="result"></param>
+        /// <param name="path"></param>
+        private void CreateNewDataset(weka.core.Instances insts, List<double[]> result, string path)
+        {
+            //Tetaları Listeye Ekle
+            List<List<string>> lst = new List<List<string>>();
+            for (int i = 0; i < insts.numInstances(); i++)
+            {
+                lst.Add(new List<string>());
+                for (int j = 0; j < insts.instance(i).numValues() - 1; j++)
+                {
+                    string value = insts.instance(i).toString(j);
+                    for (int k = 0; k < categories[j].Length; k++)
+                    {
+                        if (insts.instance(i).toString(j) == categories[j][k])
+                        {
+                            lst[lst.Count - 1].Add(String.Format("{0:0.00}", result[j][k]));
+                            break;
+                        }
+                    }
+                }
+            }
+            //Attiribute Değiştir
+            for (int i = 0; i < insts.numAttributes() - 1; i++)
+            {
+                string name = insts.attribute(i).name().ToString();
+                insts.deleteAttributeAt(i);
+                weka.core.Attribute att = new weka.core.Attribute(name);
+                insts.insertAttributeAt(att, i);
+            }
+
+            //Attiributeları yaz
+            for (int i = 0; i < insts.numInstances(); i++)
+            {
+                for (int j = 0; j < insts.instance(i).numValues() - 1; j++)
+                {
+                    insts.instance(i).setValue(j, Convert.ToDouble(lst[i][j]));
+                }
+            }
+
+            if (File.Exists(path)) File.Delete(path);
+            var saver = new ArffSaver();
+            saver.setInstances(insts);
+            saver.setFile(new java.io.File(path));
+            saver.writeBatch();
+        }
+
+
+        /// <summary>
+        /// Showing the results on the UI
+        /// </summary>
+        /// <param name="ınfo"></param>
+        private void LogInfo(string info)
+        {
+            txtInfo.Text += info;
+        }
+
+        #endregion
+
+
+    }
+}
